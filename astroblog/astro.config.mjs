511761bf09009b5fd81baef46c32fd17992df1c1
cumironin <cumironin@gmail.com>
2024-12-31 20:05:52 +0700

    add node to astro config

--- astroblog/astro.config.mjs
+++ astroblog/astro.config.mjs
@@ -7,14 +7,23 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
+
   server: {
     port: 3000,
   },
+
   output: "server",
+
   vite: {
     envPrefix: "BLOG_",
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
